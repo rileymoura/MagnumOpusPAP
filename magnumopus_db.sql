9f81689850d961f2c2b03ebec4b6f47cf28c42bd
xdmah <xdmah@DESKTOP-CBLG80E>
2021-12-02 00:48:55 +0000

    Create magnumopus_db.sql

---
+++ magnumopus_db.sql
@@ -0,0 +1,233 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 01-Dez-2021 às 21:52
+-- Versão do servidor: 5.7.31
+-- versão do PHP: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `magnumopus_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `cod_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_categoria` varchar(50) NOT NULL,
+  PRIMARY KEY (`cod_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`cod_categoria`, `nome_categoria`) VALUES
+(1, 'Guitarras'),
+(3, 'Baterias'),
+(4, 'Baixos');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `cod_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_cliente` text NOT NULL,
+  `morada` text NOT NULL,
+  `cod_postal` varchar(8) NOT NULL,
+  `localidade` varchar(50) NOT NULL,
+  `cidade` text NOT NULL,
+  `num_tel` int(9) NOT NULL,
+  PRIMARY KEY (`cod_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`cod_cliente`, `nome_cliente`, `morada`, `cod_postal`, `localidade`, `cidade`, `num_tel`) VALUES
+(3, 'Nuno Moura', 'Rua 1 , 123', '1234-567', 'Localidade', 'Cidade', 912345678);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `encomendas`
+--
+
+DROP TABLE IF EXISTS `encomendas`;
+CREATE TABLE IF NOT EXISTS `encomendas` (
+  `cod_encomenda` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_cliente` int(11) NOT NULL,
+  `preco_total` int(50) NOT NULL DEFAULT '0',
+  `data` date NOT NULL,
+  `estado` varchar(25) DEFAULT 'Em Processamento',
+  PRIMARY KEY (`cod_encomenda`),
+  KEY `cod_cliente` (`cod_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `encomendas`
+--
+
+INSERT INTO `encomendas` (`cod_encomenda`, `cod_cliente`, `preco_total`, `data`, `estado`) VALUES
+(1, 3, 80, '2021-11-15', 'Em Processamento');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `encomendas_produtos`
+--
+
+DROP TABLE IF EXISTS `encomendas_produtos`;
+CREATE TABLE IF NOT EXISTS `encomendas_produtos` (
+  `cod_encprod` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_encomenda` int(11) NOT NULL,
+  `cod_produto` int(11) NOT NULL,
+  `quant` int(11) NOT NULL,
+  `preco_prods` int(11) DEFAULT '0',
+  PRIMARY KEY (`cod_encprod`),
+  KEY `cod_produto` (`cod_produto`),
+  KEY `cod_encomenda` (`cod_encomenda`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `encomendas_produtos`
+--
+
+INSERT INTO `encomendas_produtos` (`cod_encprod`, `cod_encomenda`, `cod_produto`, `quant`, `preco_prods`) VALUES
+(2, 1, 2, 2, 30),
+(3, 1, 3, 2, 40);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_func` text NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `tipo_user` varchar(10) NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_2` (`username`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `login`
+--
+
+INSERT INTO `login` (`id`, `nome_func`, `username`, `password`, `tipo_user`) VALUES
+(1, 'Administrador', 'admin', 'adminmopus', 'admin'),
+(2, 'user', 'user', 'user', 'user'),
+(3, 'user2', 'user2', 'user2', 'user'),
+(9, '', '', '', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `cod_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_produto` varchar(50) NOT NULL,
+  `cod_categoria` int(11) NOT NULL,
+  `cod_subcategoria` int(11) NOT NULL,
+  `quant_disp` int(11) NOT NULL,
+  `preco` int(50) DEFAULT '0',
+  PRIMARY KEY (`cod_produto`),
+  KEY `cod_categoria` (`cod_categoria`),
+  KEY `cod_subcategoria` (`cod_subcategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`cod_produto`, `nome_produto`, `cod_categoria`, `cod_subcategoria`, `quant_disp`, `preco`) VALUES
+(2, 'Millenium Focus Junior Drum Set Black', 3, 3, 3, 15),
+(3, 'Harley Benton JB-75MN Black Vintage Series', 4, 4, 4, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sub_categorias`
+--
+
+DROP TABLE IF EXISTS `sub_categorias`;
+CREATE TABLE IF NOT EXISTS `sub_categorias` (
+  `cod_subcategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_subcategoria` varchar(50) NOT NULL,
+  `cod_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`cod_subcategoria`),
+  KEY `cod_categoria` (`cod_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `sub_categorias`
+--
+
+INSERT INTO `sub_categorias` (`cod_subcategoria`, `nome_subcategoria`, `cod_categoria`) VALUES
+(1, 'Guitarras Elétricas', 1),
+(3, 'Bateria Acustica', 3),
+(4, 'Baixos Elétrico', 4);
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `encomendas`
+--
+ALTER TABLE `encomendas`
+  ADD CONSTRAINT `encomendas_ibfk_1` FOREIGN KEY (`cod_cliente`) REFERENCES `clientes` (`cod_cliente`);
+
+--
+-- Limitadores para a tabela `encomendas_produtos`
+--
+ALTER TABLE `encomendas_produtos`
+  ADD CONSTRAINT `encomendas_produtos_ibfk_1` FOREIGN KEY (`cod_encomenda`) REFERENCES `encomendas` (`cod_encomenda`),
+  ADD CONSTRAINT `encomendas_produtos_ibfk_2` FOREIGN KEY (`cod_produto`) REFERENCES `produtos` (`cod_produto`);
+
+--
+-- Limitadores para a tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`cod_categoria`) REFERENCES `categorias` (`cod_categoria`),
+  ADD CONSTRAINT `produtos_ibfk_2` FOREIGN KEY (`cod_subcategoria`) REFERENCES `sub_categorias` (`cod_subcategoria`);
+
+--
+-- Limitadores para a tabela `sub_categorias`
+--
+ALTER TABLE `sub_categorias`
+  ADD CONSTRAINT `sub_categorias_ibfk_1` FOREIGN KEY (`cod_categoria`) REFERENCES `categorias` (`cod_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
